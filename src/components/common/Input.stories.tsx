8124a3f23b0e17d07c3852e119241a6a05487895
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-11-27 17:43:06 +0900

    :sparkles: [feat] Input Component

---
+++ src/components/common/Input.stories.tsx
@@ -0,0 +1,76 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Input from "./Input";
+
+const meta: Meta<typeof Input> = {
+  title: "Components/Input",
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof Input>;
+
+// 기본 Input 스토리
+export const Default: Story = {
+  args: {
+    name: "default-input",
+    placeholder: "Enter text here...",
+    type: "text",
+    width: "375px",
+    height: "54px",
+    className: "",
+  },
+};
+
+// 로그인 및 회원가입 Input
+export const AuthInput: Story = {
+  args: {
+    name: "auth-input",
+    placeholder: "Enter your info",
+    type: "text",
+    width: "375px",
+    height: "54px",
+    className: "auth", // "auth" 클래스는 흰색 배경으로 설정됨
+  },
+};
+
+// 비밀번호 Input(type=password)
+export const PasswordInput: Story = {
+  args: {
+    name: "password-input",
+    placeholder: "Enter your password",
+    type: "password",
+    width: "375px",
+    height: "54px",
+    className: "auth", // "auth" 클래스는 흰색 배경으로 설정
+  },
+};
+
+// input 내부에 value가 있을 때
+export const ValueInput: Story = {
+  args: {
+    name: "value-input",
+    placeholder: "Enter your name",
+    type: "text",
+    width: "375px",
+    height: "54px",
+    className: "",
+    value: "안녕하시렵니까",
+  },
+};
+
+// 작은 크기의 Input 필드
+export const ErrorMessageInput: Story = {
+  args: {
+    name: "none-auth-input",
+    placeholder: "Enter anything",
+    type: "text",
+    width: "375px",
+    height: "54px",
+    className: "",
+    error: "4040404040404040440404",
+  },
+};
---
+++ src/components/common/Input.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+import assets from "@/variables/images";
+
+// Error Messages 출력은 추후 수정해야함
+
+interface InputProps {
+  name: string;
+  placeholder?: string;
+  type?: string;
+  width?: string | number;
+  height?: string | number;
+  className?: string;
+  value?: string;
+  error?: string;
+  onChange?: (value: string) => void;
+}
+
+export default function Input({
+  name,
+  placeholder = "",
+  type = "text",
+  width = "auto",
+  height = "auto",
+  className = "",
+  value = "",
+  error = "",
+  onChange,
+}: InputProps): JSX.Element {
+  const [showPassword, setShowPassword] = useState(false);
+  const [inputValue, setInputValue] = useState<string>(value);
+
+  const togglePasswordVisibility = () => setShowPassword((prev) => !prev);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setInputValue(newValue);
+    onChange && onChange(newValue);
+  };
+
+  return (
+    <div
+      className={`flex flex-col `}
+      style={{
+        width: typeof width === "number" ? `${width}` : width,
+      }}
+    >
+      <div className="relative flex items-center">
+        <input
+          id={name}
+          placeholder={placeholder}
+          type={type === "password" && showPassword ? "text" : type}
+          style={{
+            width: typeof width === "number" ? `${width}px` : width,
+            height: typeof height === "number" ? `${height}px` : height,
+          }}
+          className={`input ${
+            className === "auth" ? "bg-white" : "bg-bg-200"
+          } placeholder:text-grayscale-400 border-[1px] border-line-200 rounded-[16px] p-[14px]
+          pr-10 focus:border-pr-blue-300 focus:outline-none text-black-400 text-xl font-regular`}
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        {type === "password" && (
+          <Image
+            src={
+              showPassword
+                ? assets.icons.visibilityOn
+                : assets.icons.visibilityOff
+            }
+            width={24}
+            height={24}
+            alt={showPassword ? "Hide password" : "Show password"}
+            onClick={togglePasswordVisibility}
+            className="absolute cursor-pointer right-[14px]"
+          />
+        )}
+      </div>
+      {error && (
+        <p className="text-pr-red-200 text-lg font-medium text-right mt-[4px] mr-[8px]">
+          {error}
+        </p>
+      )}
+    </div>
+  );
+}
