ce4a90b99ac825b497a2143e60a56143453af7cf
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-11-28 17:13:54 +0900

    :rewind: [rewind] src/app/page.tsx 변경사항 돌리기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,3 @@
-import SearchInput from "@/components/common/SearchInput";
-import Input from "@/components/common/Input";
-import Textarea from "@/components/common/Textarea";
-import ChatField from "@/components/common/ChatField";
-
 export default function Home() {
-  return (
-    <div className="bg-gray-600 flex justify-center items-center flex-col gap-4">
-      <SearchInput name="review" placeholder="최소 10자 이상 입력해주세요" />
-      <Input
-        name="review"
-        placeholder="최소 10자 이상 입력해주세요"
-        type="password"
-        error="에러가 발생했습니다."
-      />
-      <Textarea
-        name="review"
-        placeholder="최소 10자 이상 입력해주세요"
-        type="modal"
-      />
-      <ChatField value="안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요" />
-    </div>
-  );
+  return <div className="text-gray-">랜딩페이지입니다</div>;
 }
