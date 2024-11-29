ee1e15d6bd0ce742c5d0e402ae4ee5b92a173584
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-11-29 09:29:39 +0900

    :rewind: [rewind] src/app/page.tsx 변경사항 돌리기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import ChatField from "@/components/common/ChatField";
-
 export default function Home() {
-  return (
-    <ChatField
-      radius="24px"
-      value="텍스트출력kkkkkkkkkkkksㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ"
-      variant="primary"
-      type="custom"
-    />
-  );
+  return <div className="text-gray-">랜딩페이지입니다</div>;
 }
