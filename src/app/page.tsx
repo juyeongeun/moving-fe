303355f60ac4901d3d75f0c5be4f6f59747aa209
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-12-02 17:10:11 +0900

    :bug: [fix] 파일명 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,3 @@
-"use client";
-
-import QuoteModal from "@/components/common/QuoteModal";
-
 export default function Home() {
-  return (
-    <div className="bg-gray-800 w-full h-[100dvh]">
-      <QuoteModal
-        onClose={() => {}}
-        onSubmit={() => {}}
-        serviceType={1}
-        isDesignatedQuote={true}
-        isRejected={true}
-      />
-    </div>
-  );
+  return <div className="text-gray-">랜딩페이지입니다</div>;
 }
