1eccb1b522c5b2d86eea5622de30912220c8bde8
amberim <im.amberim@gmail.com>
2024-12-02 21:12:35 +0900

    :wrench:[Chore] 리베이스 후 랜딩 페이지 리셋

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import PendingRequestCard from "@/components/cards/PendingRequestCard";
-import MoverInfo from "@/components/common/card/MoverInfo";
-
 export default function Home() {
   return <div className="text-gray-">랜딩페이지입니다</div>;
 }
