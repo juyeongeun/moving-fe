0b6a136aa38d3213c7be911e8c362164551cce56
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-12-03 11:32:25 +0900

    :sparkles: [Feat] 지역 코드 / 지역 텍스트 출력 함수 추가

--- src/utils/utilFunctions.ts
+++ src/utils/utilFunctions.ts
@@ -1,6 +1,7 @@
 import { SERVICE_TYPES } from "@/variables/var";
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
+import { REGION_CODES, REGION_TEXTS } from "@/variables/regions";
 
 export const formatCount = (count: number) => {
   if (count > 9999) return "9,999+";
@@ -16,4 +17,8 @@ export const mapServiceType = (services: number[]) => {
 export const formatDateWithDay = (date: string | Date): string => {
   const dateObj = typeof date === "string" ? new Date(date) : date;
   return format(dateObj, "yyyy. MM. dd(E)", { locale: ko });
+};
+
+export const getRegionText = (code: keyof typeof REGION_CODES) => {
+  return REGION_TEXTS[REGION_CODES[code]];
 };
