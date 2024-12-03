f1096562f2b7d007e090cdf7785e8f0681979de1
KEN <recom54@gmail.com>
2024-12-03 15:28:20 +0900

    🗑️ [Cleanup] Remove DatePicker component from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-"use client";
-
-import DatePicker from "@/components/Quote/DatePicker";
-
 export default function Home() {
-  return (
-    <div className="text-gray-">
-      랜딩페이지입니다
-      <DatePicker />
-    </div>
-  );
+  return <div>랜딩페이지입니다</div>;
 }
