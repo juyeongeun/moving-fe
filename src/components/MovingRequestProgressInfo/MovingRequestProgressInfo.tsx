0e62e166b5f577deff7f25f24eaacf6719d2343e
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-12-04 03:22:44 +0900

    :sparkles: [Feat] 이사 요청 진행 정보 컴포넌트 추가
    이사 요청(등록) 페이지 상단에 진행 상황 표시용

---
+++ src/components/MovingRequestProgressInfo/MovingRequestProgressInfo.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+
+import ProgressBarMovingRequest from "../common/progress-bar/ProgressBarMovingRequest";
+
+interface MovingRequestProgressInfoProps {
+  maxValue: number;
+  currentValue: number;
+}
+
+export default function MovingRequestProgressInfo({
+  maxValue,
+  currentValue,
+}: MovingRequestProgressInfoProps) {
+  const MovingRequestProgressInfoClass = clsx(
+    "flex flex-col justify-center items-center gap-4",
+    "w-full h-[96px]",
+    "pc:gap-6 pc:h-[128px]"
+  );
+  const ProgressInfoTextClass = clsx(
+    "w-[327px]",
+    "text-2lg font-semibold text-black-400",
+    "pc:w-[1400px] pc:text-2xl"
+  );
+
+  return (
+    <div className={MovingRequestProgressInfoClass}>
+      <div className={ProgressInfoTextClass}>견적요청</div>
+      <ProgressBarMovingRequest
+        maxValue={maxValue}
+        currentValue={currentValue}
+      />
+    </div>
+  );
+}
