906b9c1ca58dc6c80b914d56c15813fb49c012b6
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-12-03 18:16:37 +0900

    :sparkles: [Feat] 서비스(이사 유형 관리) 관리 코드 추가
    전체를 나타내는 코드는 미정(BE와 협의 필요)

--- src/utils/utilFunctions.ts
+++ src/utils/utilFunctions.ts
@@ -1,6 +1,7 @@
 import { SERVICE_TYPES } from "@/variables/var";
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
+import { SERVICE_TEXTS } from "@/variables/service";
 
 export const formatCount = (count: number) => {
   if (count > 9999) return "9,999+";
@@ -16,4 +17,8 @@ export const mapServiceType = (services: number[]) => {
 export const formatDateWithDay = (date: string | Date): string => {
   const dateObj = typeof date === "string" ? new Date(date) : date;
   return format(dateObj, "yyyy. MM. dd(E)", { locale: ko });
+};
+
+export const getServiceText = (code: number): string => {
+  return SERVICE_TEXTS[code as keyof typeof SERVICE_TEXTS] || "서비스 미정";
 };
