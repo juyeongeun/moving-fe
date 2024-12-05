bbc5bdb0fd5a07ca4c9d422958fa9705d151607e
amberim <im.amberim@gmail.com>
2024-12-05 11:04:28 +0900

    :wrench:[Chore] clsx.ts 파일명 변경 -> cn.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import GNB from "@/components/layout/GNB";
 import localFont from "next/font/local";
 import "./globals.css";
 import QuoteGNB from "@/components/layout/QuoteGNB";
-import cn from "@/config/clsx";
+import cn from "@/config/cn";
 
 const pretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
--- src/components/common/LineSeparator.tsx
+++ src/components/common/LineSeparator.tsx
@@ -1,5 +1,5 @@
 import { cva, type VariantProps } from "class-variance-authority";
-import cn from "@/config/clsx";
+import cn from "@/config/cn";
 interface SeparatorProps {
   direction?: "vertical" | "horizontal";
   className?: string;
