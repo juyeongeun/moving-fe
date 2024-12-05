9aaf6440d9699462efc62031a404329988eb865f
amberim <im.amberim@gmail.com>
2024-12-05 16:39:12 +0900

    :sparkles:[Feat] CreatedReviewCard 컴포넌트 추가및 카드 세부 컴포넌트 수정

--- src/components/common/card/GrayLabel.tsx
+++ src/components/common/card/GrayLabel.tsx
@@ -2,25 +2,35 @@ import { cva } from "class-variance-authority";
 
 type GrayLabelProps = {
   children: React.ReactNode;
-  variant?: "border" | "solid";
+  variant?: "border" | "solid" | "none";
   size?: "fixed" | "responsive";
 };
 
 const grayLabelVariants = cva(
-  "flex-shrink-0 w-fit text-md font-medium text-grayscale-400 py-0.5 px-1.5 rounded-[4px]",
+  "flex-shrink-0 w-fit text-md font-medium rounded-[4px]",
   {
     variants: {
       variant: {
-        border: "border-solid border-[1px] border-line-100 bg-bg-200",
-        solid: "bg-bg-400 border-solid border-[1px] border-bg-400",
+        border:
+          "text-grayscale-400 border-solid border-[1px] border-line-100 bg-bg-200 py-0.5 px-1.5",
+        solid:
+          "text-grayscale-400 bg-bg-400 border-solid border-[1px] border-bg-400 py-0.5 px-1.5",
+        none: "text-grayscale-300",
       },
       size: {
         fixed: "",
-        responsive: "pc:text-2lg pc:px-2.5 pc:py-1",
+        responsive: "pc:text-2lg",
       },
     },
+    compoundVariants: [
+      {
+        variant: ["border", "solid"],
+        size: "responsive",
+        className: "pc:px-2.5 pc:py-1",
+      },
+    ],
     defaultVariants: {
-      variant: "solid",
+      variant: "none",
       size: "responsive",
     },
   }
