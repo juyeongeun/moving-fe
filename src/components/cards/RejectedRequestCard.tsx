88c322f20f4348d1506cc324a3399cec71e5156d
amberim <im.amberim@gmail.com>
2024-12-05 11:28:44 +0900

    :sparkle:[Feat] RejectedRequestCard 컴포넌트 추가

---
+++ src/components/cards/RejectedRequestCard.tsx
@@ -0,0 +1,29 @@
+import { type QuoteDetailsData } from "@/types/mover";
+import CardContainer from "../common/card/CardContainer";
+import QuoteDetails from "../common/card/QuoteDetails";
+import cn from "@/config/cn";
+
+interface RejectedRequestCardProps {
+  data: QuoteDetailsData;
+  className?: string;
+}
+
+const styles = {
+  completedOverlay:
+    "absolute inset-0 bg-[#040404A3] rounded-[16px] flex flex-col items-center justify-center text-white z-10 border-solid border-[1px] border-grayscale-300",
+  completedText: "text-lg font-semibold pc:text-2lg",
+};
+
+const RejectedRequestCard = ({ data, className }: RejectedRequestCardProps) => {
+  return (
+    <CardContainer className={cn("relative", className)}>
+      <QuoteDetails data={data} />
+
+      <div className={styles.completedOverlay}>
+        <p className={styles.completedText}>반려된 요청이에요</p>
+      </div>
+    </CardContainer>
+  );
+};
+
+export default RejectedRequestCard;
