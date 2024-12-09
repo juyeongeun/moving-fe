0cc62971afc0c744a0e417c51b2d47942a2fa9a6
amberim <im.amberim@gmail.com>
2024-12-09 17:23:37 +0900

    :recycle: [Refactor] layout 수정 및 Main 컴포넌트 추가(회색 배경인 페이지 Path로 내려줌

--- src/app/(mover)/layout.tsx
+++ src/app/(mover)/layout.tsx
@@ -1,16 +1,17 @@
+import Main from "@/components/layout/Main";
 import React from "react";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
+const GREY_BG_PATHS = ["/mover/my-quote"];
+
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div>
-      <main className="px-6 py-8 tablet:px-[72px] tablet:py-6 desktop:px-0">
-        {children}
-      </main>
-    </div>
+    <>
+      <Main grayBgPaths={GREY_BG_PATHS}>{children}</Main>
+    </>
   );
 };
 
