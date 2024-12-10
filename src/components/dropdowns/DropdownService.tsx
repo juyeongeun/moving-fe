23f466e7a741a325167127271527de8935135805
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-12-11 04:14:29 +0900

    :art: [Style] 드롭다운 디자인 수정

--- src/components/dropdowns/DropdownService.tsx
+++ src/components/dropdowns/DropdownService.tsx
@@ -30,7 +30,7 @@ export default function DropdownService({
 
   const dropdownStyles = {
     base: "relative box-border flex flex-row justify-between items-center \
-        pl-3.5 pr-2.5 w-[87px] h-9 \
+        pl-3.5 pr-2.5 w-[87px] h-[36px] \
         border-solid border-[1px] border-grayscale-100 rounded-lg \
         bg-transparent shadow-[4px_4px_10px_rgba(238,238,238,0.1)] \
         text-black-400 \
