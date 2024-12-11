fdb1fc070d0ff78c58048204364a065902bdecaf
amberim <im.amberim@gmail.com>
2024-12-11 17:41:22 +0900

    :recycle: [Refactor] ReviewMover 공용 컴포넌트로 수정으로 ReviewModal, CreateReviewCard 수정

--- src/components/cards/CreateReviewCard.tsx
+++ src/components/cards/CreateReviewCard.tsx
@@ -1,26 +1,20 @@
 import CardContainer from "../common/card/CardContainer";
-import ServiceChip from "../common/card/ServiceChip";
-import { formatDate, mapServiceType } from "@/utils/utilFunctions";
-import { ChipType } from "../common/card/ServiceChip";
-import ProfileImage from "../common/card/ProfileImage";
-import NameText from "../common/card/NameText";
-import TextWithGrayLabel from "../common/card/TextWithGrayLabel";
-import LineSeparator from "../common/LineSeparator";
 import Button from "../common/Button";
 import cn from "@/config/cn";
+import ReviewMover, { type ReviewMoverData } from "../common/card/ReviewMover";
 
-interface CreateReviewData {
-  id: number;
-  service: number;
-  isDesignated: boolean;
-  imageUrl: string | null;
-  nickname: string;
-  movingDate: string;
-  cost: number;
-}
+// export interface CreateReviewData {
+//   id: number;
+//   service: number;
+//   isDesignated: boolean;
+//   imageUrl: string | null;
+//   nickname: string;
+//   movingDate: string;
+//   cost: number;
+// }
 
 interface CreateReviewCardProps {
-  data: CreateReviewData;
+  data: ReviewMoverData;
   className?: string;
   onPrimaryClick: () => void;
 }
@@ -30,29 +24,9 @@ const CreateReviewCard = ({
   className,
   onPrimaryClick,
 }: CreateReviewCardProps) => {
-  const serviceType = mapServiceType([data.service])[0];
-  const moveInDate = formatDate(data.movingDate);
-  const quotePrice = `${data.cost.toLocaleString()}원`;
-
   return (
     <CardContainer className={cn("pc:py-[32px]", className)}>
-      <div className="flex gap-2">
-        <ServiceChip variant={serviceType as ChipType} />
-        {data.isDesignated && <ServiceChip variant="designatedQuote" />}
-      </div>
-
-      <div className="flex items-center gap-2.5 pc:gap-6 pc:py-2">
-        <ProfileImage imgUrl={data.imageUrl} />
-        <div className="flex flex-col gap-1 pc:gap-2">
-          <NameText text={data.nickname} type="mover" />
-          <div className="flex gap-2 items-center pc:gap-[12.5px]">
-            <TextWithGrayLabel label="이사일" text={moveInDate} />
-            <LineSeparator />
-            <TextWithGrayLabel label="견적가" text={quotePrice} />
-          </div>
-        </div>
-      </div>
-
+      <ReviewMover data={data} />
       <Button onClick={onPrimaryClick}>리뷰 작성하기</Button>
     </CardContainer>
   );
--- src/components/modals/ReviewModal.tsx
+++ src/components/modals/ReviewModal.tsx
@@ -1,36 +1,30 @@
 "use client";
 
 import Textarea from "../common/Textarea";
-import ReviewMover from "../common/card/ReviewMover";
+import ReviewMover, { type ReviewMoverData } from "../common/card/ReviewMover";
 import assets from "@/variables/images";
 import Image from "next/image";
 import Button from "../common/Button";
-import StarRating from "../common/StartRating";
-import ServiceChip from "../common/card/ServiceChip";
+import StarRating from "../common/StarRating";
 import LineSeparator from "../common/LineSeparator";
 import { useState } from "react";
 import toast from "react-hot-toast";
 
+const styles = {
+  lineSeparator: "my-[20px] pc:my-[32px]",
+  label: "text-lg font-semibold text-black-300 mb-[16px] pc:text-xl",
+};
+
 interface ReviewModalProps {
   onClose: () => void;
   onSubmit: () => void;
-  serviceType: number;
-  isDesignatedQuote: boolean;
-  moverName: string;
-  moverImageUrl: string | null;
-  moveDate: string;
-  cost: number;
+  data: ReviewMoverData;
 }
 
 export default function ReviewModal({
   onClose,
   onSubmit,
-  serviceType,
-  isDesignatedQuote = true,
-  moverName,
-  moverImageUrl,
-  moveDate,
-  cost,
+  data,
 }: ReviewModalProps) {
   const [rating, setRating] = useState<number>(0);
   const [review, setReview] = useState<string>("");
@@ -38,35 +32,6 @@ export default function ReviewModal({
   const [previews, setPreviews] = useState<string[]>([]);
 
   const isValid = rating > 0 && review.length >= 10;
-
-  const styles = {
-    container: `flex flex-col bg-white rounded-t-[32px] pt-[32px] pb-[40px]
-    pc:w-[608px] pc:rounded-[32px]`,
-    titleContainer: `text-2lg font-bold text-black-400 flex justify-between items-center mb-[26px]
-    pc:text-2xl pc:font-semibold pc:mb-[40px]`,
-    closeIcon: `cursor-pointer pc:w-[32px] pc:h-[32px]`,
-    chipsContainer: `flex flex-wrap gap-[8px] mb-[14px] pc:mb-[24px]`,
-    ratingContainer: `flex flex-wrap gap-[8px] pb-[20px]
-    pc:mb-[32px] pc:pb-[32px]`,
-    title: `text-lg font-semibold text-black-300 mb-[16px]
-    pc:text-xl`,
-    reviewContainer: `mb-[26px] pc:mb-[40px]`,
-    imageContainer: "relative w-[100px] h-[100px]",
-    reviewImage: "rounded-[8px] object-cover w-full h-full",
-    removeButton:
-      "absolute top-[4px] right-[4px] bg-black-400 rounded-full p-[4px] cursor-pointer",
-    addImageButton:
-      "w-[100px] h-[100px] border-[1px] border-gray-200 rounded-[8px] flex items-center justify-center border-[1px] border-gray-200 border-solid cursor-pointer",
-    imagesWrapper: "flex gap-[16px] mb-[20px]",
-    lineSeparator: "mb-[20px]",
-  };
-
-  const service: "smallMove" | "homeMove" | "officeMove" =
-    serviceType === 2
-      ? "homeMove"
-      : serviceType === 3
-      ? "officeMove"
-      : "smallMove";
 
   const handleRatingChange = (rating: number) => {
     setRating(rating);
@@ -100,55 +65,66 @@ export default function ReviewModal({
     setPreviews((prev) => prev.filter((_, i) => i !== index));
   };
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit();
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.titleContainer}>
-        <p>리뷰 쓰기</p>
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col bg-white w-full rounded-t-[32px] pt-[32px] pb-[40px] px-6
+  tablet:rounded-[32px] tablet:w-[375px] pc:w-[608px]"
+    >
+      <div
+        className="text-2lg font-bold text-black-400 flex justify-between items-center mb-[26px]
+  pc:text-2xl pc:font-semibold pc:mb-[40px]"
+      >
+        <h3 className="text-lg font-semibold text-black-300 pc:text-2xl">
+          리뷰 쓰기
+        </h3>
         <Image
           src={assets.icons.x}
           alt="close"
           width={24}
           height={24}
-          className={styles.closeIcon}
+          className="cursor-pointer pc:w-[32px] pc:h-[32px]"
           onClick={onClose}
         />
       </div>
-      <div className={styles.chipsContainer}>
-        <ServiceChip variant={service} />
-        {isDesignatedQuote && <ServiceChip variant="designatedQuote" />}
+
+      <div className="flex flex-col gap-4 pc:gap-6">
+        <ReviewMover data={data} variant="solid" />
       </div>
-      <ReviewMover
-        moverName={moverName}
-        moverImageUrl={moverImageUrl}
-        moveDate={moveDate}
-        cost={cost}
-      />
-      <p className={styles.title}>평점을 선택해 주세요</p>
-      <div className={styles.ratingContainer}>
+      <LineSeparator direction="horizontal" className={styles.lineSeparator} />
+      <p className={styles.label}>평점을 선택해 주세요</p>
+      <div className="flex flex-wrap gap-[8px]">
         <StarRating onRatingChange={handleRatingChange} />
       </div>
       <LineSeparator direction="horizontal" className={styles.lineSeparator} />
-      <p className={styles.title}>리뷰 이미지 첨부</p>
-      <div className={styles.imagesWrapper}>
+      <p className={styles.label}>리뷰 이미지 첨부</p>
+      <div className="flex gap-[16px] mb-[20px]">
         {previews.map((preview, index) => (
-          <div key={index} className={styles.imageContainer}>
+          <div key={index} className="relative w-[100px] h-[100px]">
             <Image
               src={preview}
               alt={`review-image-${index}`}
               width={100}
               height={100}
-              className={styles.reviewImage}
+              className="rounded-[8px] object-cover w-full h-full"
             />
             <button
-              className={styles.removeButton}
+              className="absolute top-[4px] right-[4px] bg-black-400 rounded-full p-[4px] cursor-pointer"
               onClick={() => handleRemoveImage(index)}
+              aria-label={`Remove image ${index + 1}`}
             >
               <Image src={assets.icons.x} alt="remove" width={16} height={16} />
             </button>
           </div>
         ))}
         {images.length < 3 && (
-          <label className={styles.addImageButton}>
+          <label className="w-[100px] h-[100px] border-solid border-[1px] border-gray-200 rounded-[8px] flex items-center justify-center cursor-pointer">
+            <span className="sr-only">Add image</span>
             <input
               type="file"
               accept="image/*"
@@ -165,20 +141,22 @@ export default function ReviewModal({
         )}
       </div>
       <LineSeparator direction="horizontal" className={styles.lineSeparator} />
-      <div className={styles.reviewContainer}>
-        <p className={styles.title}>상세 후기를 작성해 주세요</p>
-        <Textarea
-          placeholder="최소 10자 이상 입력해주세요"
-          value={review}
-          onChange={handleReviewChange}
-        />
-      </div>
+
+      <p className={styles.label}>상세 후기를 작성해 주세요</p>
+      <Textarea
+        placeholder="최소 10자 이상 입력해주세요"
+        value={review}
+        onChange={handleReviewChange}
+      />
       <Button
-        children="리뷰 작성하기"
         variant="primary"
         onClick={onSubmit}
+        type="submit"
         disabled={!isValid}
-      />
-    </div>
+        className="mt-[26px] pc:mt-[40px]"
+      >
+        리뷰 작성하기
+      </Button>
+    </form>
   );
 }
