9430bb652dd1760d4a031d1d99c7cfbe7954a1d1
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-12-13 17:05:57 +0900

    :recycle: [Refactor] 경로 수정

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,5 +1,5 @@
 import { QuoteData } from "./quote";
-import { MovingRequestDataWithComplete } from "./moving-request";
+import { MovingRequestDataWithComplete } from "./movingRequest";
 import { MoverDetailData } from "./mover";
 
 // API 구조 참고
