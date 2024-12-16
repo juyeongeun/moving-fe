b023719c6c30599d73368d57c239f4fcbd5ed7ef
amberim <im.amberim@gmail.com>
2024-12-16 18:30:11 +0900

    :wrench: [Chore] mocks/data quote, movingRequest 목 데이터 추가

--- src/mocks/handlers/quote.ts
+++ src/mocks/handlers/quote.ts
@@ -1,8 +1,25 @@
 import { http, HttpResponse } from "msw";
+import {
+  SENT_QUOTE_LIST,
+  REJECTED_QUOTE_LIST,
+  SENT_QUOTE_DETAIL,
+} from "../data/quote";
 
 export const quoteHandlers = [
-  //예시
-  // http.get("/api/reviews/me", () => {
-  //   return HttpResponse.json(목데이터);
-  // }),
+  http.get("/api/quotes/mover", () => {
+    return HttpResponse.json(SENT_QUOTE_LIST);
+  }),
+  http.get("/api/quotes/mover/rejected", () => {
+    return HttpResponse.json(REJECTED_QUOTE_LIST);
+  }),
+  http.get("/api/quotes/mover/:quoteId", ({ params }) => {
+    const { quoteId } = params;
+    const quote = SENT_QUOTE_DETAIL;
+
+    if (!quote) {
+      return new HttpResponse(null, { status: 404 });
+    }
+
+    return HttpResponse.json(quote);
+  }),
 ];
