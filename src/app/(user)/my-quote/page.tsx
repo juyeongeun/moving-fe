7746e15ed25112bd592d49e928e2dc1b6f742395
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-12-18 18:33:29 +0900

    :recycle: [Refactor] 안 쓰는 파일/코드 삭제

--- src/app/(user)/my-quote/page.tsx
+++ src/app/(user)/my-quote/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import assets from "@/variables/images";
-import Image from "next/image";
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import PendingRequestCard, {
