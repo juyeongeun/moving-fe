d9685fe9db4cca9b646e38a216c992d7e5c0bc9f
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-12-27 15:08:50 +0900

    :bug: [fix] gnb 오류 수정

--- src/app/(mover)/layout.tsx
+++ src/app/(mover)/layout.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import Main from "@/components/layout/Main";
 import React from "react";
 import RoleGuard from "@/components/auth/RoleGuard";
+import { useAuth } from "@/hooks/useAuth";
+
 interface LayoutProps {
   children: React.ReactNode;
 }
@@ -8,6 +12,7 @@ interface LayoutProps {
 const GREY_BG_PATHS = ["/mover/my-quote"];
 
 const Layout = ({ children }: LayoutProps) => {
+  useAuth();
   return (
     <>
       <RoleGuard allowedRoles={"MOVER"}>
